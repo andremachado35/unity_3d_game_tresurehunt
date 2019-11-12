cc7b8269027c3e5adb3e953e3be002a0e2ad8d57
André Machado <a66693@alunos.uminho.pt>
2019-11-12 14:23:07 +0000

    final - beta

---
+++ treasurehunt/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Ethan;
+
+    private Vector3 offset;
+
+    void Start ()
+    {
+        offset = transform.position - Ethan.transform.position;
+    }
+
+    void LateUpdate ()
+    {
+        transform.position = Ethan.transform.position + offset;
+    }
+
+}
